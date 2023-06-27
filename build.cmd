c908cadd3fc8cb57b0f84b10bdc656a3e2aa62fb
Codenade <amadeus.dorian04@gmail.com>
2023-06-27 18:40:50 +0200

    * delete build.cmd -> new file: build.bat

--- build.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Build
-dotnet build
-echo Plugin build finished
-echo Building Addressables
-"%ProgramFiles%\Unity\Hub\Editor\2020.3.33f1\Editor\Unity.exe" -projectPath "%CD%\ksp2-inputbinder-assets\" -quit -batchmode -executeMethod BuildAssets.PerformBuild
-xcopy "%CD%\ksp2-inputbinder-assets\Library\com.unity.addressables\aa\Windows\" "%CD%\build\GameData\Mods\inputbinder\addressables\" /Y /I /E
-echo Done
