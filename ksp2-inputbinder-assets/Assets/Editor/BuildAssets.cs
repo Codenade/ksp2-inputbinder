b2bddb257361235f758dde863a7b1c37759e9c10
Codenade <amadeus.dorian04@gmail.com>
2023-10-11 23:28:41 +0200

    update build process, add actions

--- ksp2-inputbinder-assets/Assets/Editor/BuildAssets.cs
+++ ksp2-inputbinder-assets/Assets/Editor/BuildAssets.cs
@@ -1,8 +1,37 @@
+#if UNITY_EDITOR
+
+using System;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
 public static class BuildAssets
 {
+    private static readonly string Eol = Environment.NewLine;
+
+    [MenuItem("Build/Addressables")]
     public static void PerformBuild()
     {
         UnityEditor.AddressableAssets.Settings.AddressableAssetSettings.CleanPlayerContent();
         UnityEditor.AddressableAssets.Settings.AddressableAssetSettings.BuildPlayerContent();
+        ReportSummary(new BuildSummary());
+    }
+
+	// This is a workaround for game-ci/build falsely reporting ERROR when this is not done
+    private static void ReportSummary(BuildSummary summary)
+    {
+        Console.WriteLine(
+            $"{Eol}" +
+            $"###########################{Eol}" +
+            $"#      Build results      #{Eol}" +
+            $"###########################{Eol}" +
+            $"{Eol}" +
+            $"Duration: {summary.totalTime}{Eol}" +
+            $"Warnings: {summary.totalWarnings}{Eol}" +
+            $"Errors: {summary.totalErrors}{Eol}" +
+            $"Size: {summary.totalSize} bytes{Eol}" +
+            $"{Eol}"
+        );
     }
 }
+
+#endif
