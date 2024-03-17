ababa8d5e86d085ebf0fada42573595119bada2f
Codenade <amadeus.dorian04@gmail.com>
2024-03-17 20:15:31 +0100

    implement search function

--- ksp2-inputbinder/ui/BindingGroup.cs
+++ ksp2-inputbinder/ui/BindingGroup.cs
@@ -11,9 +11,11 @@ namespace Codenade.Inputbinder
         private InputAction _action;
         private TextMeshProUGUI _pathTxt;
         private TextMeshProUGUI _procText;
+        private string _toMark;
 
         public void Initialize(InputAction action, int bindingIndex)
         {
+            _toMark = null;
             _action = action;
             _bindingIndex = bindingIndex;
             var bindingInfoGroup = gameObject.GetChild("BindingInfoGroup");
@@ -34,9 +36,32 @@ namespace Codenade.Inputbinder
             modifyBindingGroup.GetChild("ClearButton").GetComponent<Button>().onClick.AddListener(OnClearClicked);
         }
 
+        public void Initialize(InputAction action, int bindingIndex, string toMark)
+        {
+            _toMark = toMark;
+            _action = action;
+            _bindingIndex = bindingIndex;
+            var bindingInfoGroup = gameObject.GetChild("BindingInfoGroup");
+            var modifyBindingGroup = gameObject.GetChild("ModifyBindingGroup");
+            bindingInfoGroup.GetChild("BindingName").GetComponent<TextMeshProUGUI>().text = action.bindings[bindingIndex].name;
+            _pathTxt = bindingInfoGroup.GetChild("BindingPath").GetComponent<TextMeshProUGUI>();
+            _pathTxt.text = Utils.MarkText(action.bindings[bindingIndex].effectivePath, toMark);
+            _procText = bindingInfoGroup.GetChild("BindingProcessors").GetComponent<TextMeshProUGUI>();
+            _procText.text = action.bindings[bindingIndex].effectiveProcessors;
+            if (action.bindings[bindingIndex].isPartOfComposite)
+            {
+                modifyBindingGroup.GetChild("ProcessorButton").SetActive(false);
+                bindingInfoGroup.GetChild("BindingProcessors").SetActive(false);
+            }
+            else
+                modifyBindingGroup.GetChild("ProcessorButton").GetComponent<Button>().onClick.AddListener(OnModifyClicked);
+            modifyBindingGroup.GetChild("RebindButton").GetComponent<Button>().onClick.AddListener(OnRebindClicked);
+            modifyBindingGroup.GetChild("ClearButton").GetComponent<Button>().onClick.AddListener(OnClearClicked);
+        }
+
         private void Update()
         {
-            _pathTxt.text = _action.bindings[_bindingIndex].effectivePath;
+            _pathTxt.text = Utils.MarkText(_action.bindings[_bindingIndex].effectivePath, _toMark);
             _procText.text = _action.bindings[_bindingIndex].effectiveProcessors;
         }
 
